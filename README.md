6f5e945ec10c436f47cd5f24e7d254d96939c935
Rizki Esa01~ <rizky.esa@bankmega.com>
2023-06-30 13:12:31 +0700

    'first'

---
+++ README.md
@@ -0,0 +1,16 @@
+# Save changes in Family Tree JS with Node.js
+
+## Download or clone the project and run this do install the packages:
+```
+npm install
+```
+
+## To add <strong>Family Tree JS</strong>:
+Create a folder called <strong>js</strong> in the <strong>/public</strong> folder.
+
+[Download](https://familytreejs.balkan.app/js/get/free) FamilyTreeJS and add extracted <strong>orgchart.js</strong> file into your <strong>/public/js</strong> folder
+
+## To start the project run this:
+```
+node app
+```
---
+++ app.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => {
+  fs.readFile('./db.json', 'utf8', (err, json) => {
+    if (err) {
+      console.log(err);
+    } else {
+        res.render('index', { json });
+    }
+  });
+});
+
+app.post('/', (req, res) => {
+  fs.readFile('./db.json', 'utf8', (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      let nodes = JSON.parse(data);
+      req.body.addNodesData.forEach(node => {
+        nodes.push(node);
+      });
+      req.body.updateNodesData.forEach(node => {
+        const index = nodes.findIndex((n) => n.id === node.id);
+        nodes[index] = node;
+      });
+      nodes = nodes.filter((node) => node.id !== req.body.removeNodeId);
+      fs.writeFile('./db.json', JSON.stringify(nodes), {flag:'w'}, (err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.send(nodes);
+        }
+      });
+    }
+  });
+});
+
+app.listen(port, () => console.log(`App listening on port ${port}!`));
+
+module.exports = app;
